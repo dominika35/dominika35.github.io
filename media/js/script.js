a94b80ea9481ffa054c5f61095bb80a127ea5183
Piotr Klupa <piiter.klupcia@gmail.com>
2016-01-13 21:58:47 +0100

    New page and subpages

---
+++ media/js/script.js
@@ -0,0 +1,14 @@
+document.getElementById('top').onclick = function () {
+    scrollTo(document.body, 0, 100);
+}
+
+function scrollTo(element, to, duration) {
+  if (duration < 0) return;
+  var difference = to - element.scrollTop,
+    perTick = difference / duration * 2;
+
+  setTimeout(function() {
+      element.scrollTop = element.scrollTop + perTick;
+      scrollTo(element, to, duration - 2);
+  }, 10);
+}
